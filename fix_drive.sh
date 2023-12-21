ee58d7938c826c4dc081d1b9c93e5b1329022d68
Af7eR9l0W <85209771+Af7eR9l0W@users.noreply.github.com>
2023-12-21 16:45:23 -0600

    v1.0 for fix_drive.sh
    Added colors, fixed issues and minified a bit

--- fix_drive.sh
+++ fix_drive.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "ntfsfix requires sudo"
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${CYAN}ntfsfix${NC} requires sudo"
 sleep 1
 echo "please don't run the script unless you know what you're doing"
 sleep .5
@@ -8,45 +13,45 @@ echo "----------------------------------------------"
 
 if ! command -v ntfsfix &> /dev/null
 then
-    echo "ntfsfix could not be found. Please install it and try again."
+    echo -e "${CYAN}ntfsfix${NC} ${RED}could not be found.${NC} ${RED}Please install it and try again.${NC}"
     exit 1
 fi
 
 if [ "$EUID" -ne 0 ]
 then
-    echo "Please run this script with root privileges."
+    echo -e "${RED}Please run this script with root privileges.${NC}"
     exit 1
 fi
 
-echo "Please enter the drive you want to use (e.g., /dev/sda1):"
+echo -e "${CYAN}Please enter the drive you want to use${NC} ${GREEN}(e.g., /dev/sda1):${NC}"
 read user_drive
 
 if [ ! -b "$user_drive" ]
 then
-    echo "Invalid drive. Please enter a valid drive."
+    echo -e "${RED}Invalid drive.${NC} ${GREEN}Please enter a valid drive.${NC}"
     exit 1
 fi
 
 if [ ! -e "$user_drive" ]
 then
-    echo "Drive does not exist. Please enter a valid drive."
+    echo -e "${RED}Drive does not exist.${NC} ${GREEN}Please enter a valid drive.${NC}"
     exit 1
 fi
 
-echo "You are about to run ntfsfix on $user_drive. Are you sure? (y/n)"
+echo -e "${GREEN}You are about to run${NC} ${CYAN}ntfsfix${NC} ${GREEN}on $user_drive. ${CYAN}Are you sure?${NC} ${RED}(y/n)${NC}"
 read confirm
 
 if [ "$confirm" != "y" ]
 then
-    echo "Operation cancelled."
+    echo -e "${RED}Operation cancelled."
     exit 0
 fi
 
 if ! sudo ntfsfix -d "$user_drive"
 then
-    echo "ntfsfix failed. Please check the drive and try again."
+    echo -e "${RED}ntfsfix failed. Please check the drive and try again.${NC}"
     exit 1
 fi
 
-echo "ntfsfix completed successfully."
+echo -e "${CYAN}ntfsfix ${GREEN}completed successfully."
 exit 0
